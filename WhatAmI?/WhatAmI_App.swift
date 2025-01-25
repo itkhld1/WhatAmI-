d602821187f99a465a9851a20c1ee8f5cdc1b928
Khaled Samim <126877550+itkhld1@users.noreply.github.com>
2025-01-25 13:54:19 +0300

    Initial Commit

---
+++ WhatAmI?/WhatAmI_App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatAmI_App.swift
+//  WhatAmI?
+//
+//  Created by itkhld on 25.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatAmI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
